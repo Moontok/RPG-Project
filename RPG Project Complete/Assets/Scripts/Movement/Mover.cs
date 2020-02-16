0dfdaaa1f7f278fb4434c05268ea8b9ad00e6d80
Moontok <50626466+Moontok@users.noreply.github.com>
2020-02-15 21:50:55 -0600

    CombatStart

---
+++ RPG Project Complete/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    
+    void Update()
+    {
+        UpdateAnimator();
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        this.GetComponent<NavMeshAgent>().destination = destination;
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = this.GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = this.transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+
+        this.GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
+}
