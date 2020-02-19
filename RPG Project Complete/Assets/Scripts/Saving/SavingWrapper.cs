9b7a9ab49110aedaff19dae1d42907ead9cfe61b
Moontok <zspink@nea.k12.ar.us>
2020-02-19 15:56:39 -0600

    SavingSystem2_19

---
+++ RPG Project Complete/Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        private void Update() 
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                this.GetComponent<SavingSystem>().Save(defaultSaveFile);
+            }
+            if (Input.GetKeyDown(KeyCode.F9))
+            {
+                this.GetComponent<SavingSystem>().Load(defaultSaveFile);
+            }
+        }
+    }
+}
--- RPG Project Complete/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG Project Complete/Assets/Scripts/SceneManagement/Portal.cs
@@ -16,6 +16,10 @@ namespace RPG.SceneManagement
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint = null;
         [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeWaitTime = 2f;
+        
 
         private void OnTriggerEnter(Collider other) 
         {
@@ -34,9 +38,17 @@ namespace RPG.SceneManagement
             }
 
             DontDestroyOnLoad(this.gameObject);
+
+            Fader fader = FindObjectOfType<Fader>();
+
+            yield return fader.FadeOut(fadeOutTime);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeOutTime);
+            yield return fader.FadeIn(fadeInTime);
 
             Destroy(this.gameObject);
         }
