82d2a8e06d96ce12489bd411c9a41200d58274de
Moontok <zspink@nea.k12.ar.us>
2020-03-17 15:45:47 -0500

    Inv Implementation

---
+++ RPG_Project_Complete/Assets/Scripts/Control/ClickablePickup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    [RequireComponent(typeof(Pickup))]
+    public class ClickablePickup : MonoBehaviour, IRaycastable
+    {
+        Pickup pickup = null;
+
+        private void Awake()
+        {
+            pickup = this.GetComponent<Pickup>();
+        }
+
+        public CursorType GetCursorType()
+        {
+            if (pickup.CanBePickedUp())
+            {
+                return CursorType.Pickup;
+            }
+            else
+            {
+                return CursorType.FullPickup;
+            }
+        }
+
+        public bool HandleRaycast(PlayerController callingController)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                pickup.PickupItem();
+            }
+            return true;
+        }        
+    }
+}
