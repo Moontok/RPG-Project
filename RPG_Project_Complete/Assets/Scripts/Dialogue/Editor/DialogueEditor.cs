f96e0008b10a9f3864ee213b059ca7354bc65a0a
Moontok <zspink@nea.k12.ar.us>
2020-09-24 15:51:14 -0500

    Started Dialogue Editor

---
+++ RPG_Project_Complete/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace RPG.Dialogue.Editor
+{
+    public class DialogueEditor : EditorWindow
+    {
+        [MenuItem("Window/Dialogue Editor")]
+        public static void ShowEditorWindow()
+        {
+            GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            
+            if(dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
