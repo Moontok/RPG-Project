1d0593f1c862c05afa2a69060ddcc6a4147f09a7
Moontok <zspink@nea.k12.ar.us>
2020-10-21 10:53:07 -0500

    Player Dialogue UI

---
+++ RPG_Project_Complete/Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] Dialogue currentDialogue = null;
+        DialogueNode currentNode = null;
+
+        void Awake() 
+        {
+            currentNode = currentDialogue.GetRootNode();
+        }
+
+        public string GetText()
+        {
+            if(currentNode == null)
+            {
+                return "";
+            }
+
+            return currentNode.GetText();
+        }
+
+        public void Next()
+        {
+            DialogueNode[] children = currentDialogue.GetAllChidren(currentNode).ToArray();
+            int randomIndex = Random.Range(0, children.Count());
+            currentNode = children[randomIndex];
+        }
+
+        public bool HasNext()
+        {
+            return currentDialogue.GetAllChidren(currentNode).Count() > 0;
+        }
+    }
+}
---
+++ RPG_Project_Complete/Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Dialogue;
+using TMPro;
+using UnityEngine.UI;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant = null;
+        [SerializeField] TextMeshProUGUI AIText = null;
+        [SerializeField] Button nextButton = null;
+
+        void Start() 
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            nextButton.onClick.AddListener(Next);
+
+            UpdateUI();
+        }
+
+        void Next()
+        {
+            playerConversant.Next();
+            UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
+        }
+    }
+}
