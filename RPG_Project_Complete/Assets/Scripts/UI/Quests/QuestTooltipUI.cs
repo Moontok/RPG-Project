6c21eb4d165c207ac3bc6b96abc349ad690f28f7
Moontok <zspink@nea.k12.ar.us>
2020-11-01 22:06:15 -0600

    ++

--- RPG_Project_Complete/Assets/Scripts/UI/Quests/QuestTooltipUI.cs
+++ RPG_Project_Complete/Assets/Scripts/UI/Quests/QuestTooltipUI.cs
@@ -23,16 +23,16 @@ namespace RPG.UI.Quests
                 Destroy(child.gameObject);
             }
 
-            foreach (string objective in quest.GetObjectives())
+            foreach (var objective in quest.GetObjectives())
             {
                 GameObject prefab = objectiveIncompletePrefab;
-                if(status.IsObjectiveComplete(objective))
+                if(status.IsObjectiveComplete(objective.reference))
                 {
                     prefab = objectivePrefab;
                 }
                 GameObject objectiveInstance = Instantiate(prefab, objectiveContainer);
                 TextMeshProUGUI objectiveText = objectiveInstance.GetComponentInChildren<TextMeshProUGUI>();
-                objectiveText.text = objective;
+                objectiveText.text = objective.description;
             }
         }
     }
